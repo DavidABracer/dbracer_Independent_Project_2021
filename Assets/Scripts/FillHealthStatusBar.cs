8e4fd671a564c4a555acf4af305bb29c0a6ed322
DavidABracer <davidbracer@yahoo.com>
2021-05-29 19:23:39 -0500

    Changes Made in the Health System and Scripts
    Changes Made in the Health System and Scripts

---
+++ Assets/Scripts/FillHealthStatusBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FillHealthStatusBar : MonoBehaviour
+{
+    public Health Health;
+    public Image fillImage;
+    private Slider slider;
+
+    void Awake()
+    {
+        slider = GetComponent<Slider>();  
+    }
+
+    void update()
+    
+    {
+        float fillvalue = Health.StartingHealth / Health.StartingHealth; 
+    }
+}
